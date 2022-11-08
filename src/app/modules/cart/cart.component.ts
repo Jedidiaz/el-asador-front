f79f96dd4029bedec8b5eed24482bdd143ea638b
Jedidiaz <jedidiazfagundez@gmail.com>
2022-11-08 16:50:06 -0500

    modificaciones al carrito de compras

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-cart',
@@ -7,75 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  productsCart:any[] = [];
+  name = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  productsCart:any[] = [{
+    id: 1,
+    name: this.name,
+    count: 2,
+    image: './image-test.jpg',
+    price: 50,
+    cateorgy: 'foot',
+    color: 'White',
+    size: 'Large'
+  },{
+    id: 1,
+    name: this.name,
+    count: 2,
+    image: './image-test.jpg',
+    price: 50,
+    cateorgy: 'foot',
+    color: 'White',
+    size: 'Large'
+  },{
+    id: 1,
+    name: this.name,
+    count: 2,
+    image: './image-test.jpg',
+    price: 50,
+    cateorgy: 'foot',
+    color: 'White',
+    size: 'Large'
+  },{
+    id: 1,
+    name: this.name,
+    count: 2,
+    image: './image-test.jpg',
+    price: 50,
+    cateorgy: 'foot',
+    color: 'White',
+    size: 'Large'
+  },{
+    id: 1,
+    name: this.name,
+    count: 2,
+    image: './image-test.jpg',
+    price: 50,
+    cateorgy: 'foot',
+    color: 'White',
+    size: 'Large'
+  }];
   productSelect:any;
 
   count:any[] = [];
   countSelect:any;
-
-
   subtotal:number = 0;
-  shipping:number = 0;
-  tax:number = 0;
-  total:number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.productsCart.push({
-      id: 1,
-      name: 'Shirt',
-      count: 2,
-      image: './image-test.jpg',
-      price: 50,
-      cateorgy: 'foot',
-      color: 'White',
-      size: 'Large'
-    },{
-      id: 1,
-      name: 'Shirt',
-      count: 2,
-      image: './image-test.jpg',
-      price: 50,
-      cateorgy: 'foot',
-      color: 'White',
-      size: 'Large'
-    },{
-      id: 1,
-      name: 'Shirt',
-      count: 2,
-      image: './image-test.jpg',
-      price: 50,
-      cateorgy: 'foot',
-      color: 'White',
-      size: 'Large'
-    },{
-      id: 1,
-      name: 'Shirt',
-      count: 2,
-      image: './image-test.jpg',
-      price: 50,
-      cateorgy: 'foot',
-      color: 'White',
-      size: 'Large'
-    },{
-      id: 1,
-      name: 'Shirt',
-      count: 2,
-      image: './image-test.jpg',
-      price: 50,
-      cateorgy: 'foot',
-      color: 'White',
-      size: 'Large'
-    });
-
     this.count.push({name: '1', code: '1'},{name: '2', code: '2'},{name: '3', code: '3'},{name: '4', code: '4'});
   }
 
   cancel(product:any){
     let temp = this.productsCart.find(el => el.id == product.id);
 
-    console.log(product)
+    console.log(product);
 
     if(temp != null){
       this.productsCart.pop();
